d1f6639c817d196b17c9caebe947d750a898f650
Sanhita207 <sanhita.dhamdhere5@gmail.com>
2020-11-17 12:02:37 -0600

    Completed initial setup of the board

--- Board.py
+++ Board.py
@@ -19,6 +19,51 @@ class Board:
 
         self.board = [[BoardCell() for _col in range(self.ncols)] for _row in range(self.nrows)]
 
+        self.set_invalid_points_on_board()
+
+        self.fox_collection = {}
+        self.geese_collection = {}
+        self.elephant_collection = {}
+
+        self.set_foxes_initial_position()
+        self.set_elephant_initial_position()
+        self.set_goose_initial_position()
+
+    def set_foxes_initial_position(self):
+        self.board[3][2].cell_value = 'F'
+        self.fox_collection['fox_1'] = (3, 2)
+
+        self.board[3][4].cell_value = 'F'
+        self.fox_collection['fox_2'] = (3, 4)
+
+        self.print_board_cell_value()
+        print(self.fox_collection)
+
+    def set_elephant_initial_position(self):
+        self.board[4][0].cell_value = 'E'
+        self.elephant_collection['ele_1'] = (4, 0)
+
+        self.board[4][6].cell_value = 'E'
+        self.elephant_collection['ele_2'] = (4, 6)
+
+        self.board[6][3].cell_value = 'E'
+        self.elephant_collection['ele_2'] = (6, 3)
+
+        self.print_board_cell_value()
+        print(self.elephant_collection)
+
+    def set_goose_initial_position(self):
+        count = 1
+        for i in range(4, self.nrows, 1):
+            for j in range(self.ncols):
+                if self.board[i][j].is_valid_cell and self.board[i][j].cell_value is None:
+                    self.board[i][j].cell_value = 'G'
+                    self.geese_collection['ge_{}'.format(count)] = (i, j)
+                    count += 1
+
+        self.print_board_cell_value()
+        print(self.geese_collection)
+
     def set_invalid_points_on_board(self):
 
         for i in range(self.nrows):
@@ -36,6 +81,12 @@ class Board:
                 print(self.board[i][j].is_valid_cell, end=" ")
             print("")
 
+    def print_board_cell_value(self):
+        for i in range(self.nrows):
+            for j in range(self.ncols):
+                print(self.board[i][j].cell_value, end=" ")
+            print("")
+
 
 board = Board()
-board.set_invalid_points_on_board()
+# board.set_invalid_points_on_board()
