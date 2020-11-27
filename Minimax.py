9ce9ad0560771b01b54e250ec3fdff1b1eb18b8d
Adarsh Agarwal <agarwaladarsh95@gmail.com>
2020-11-27 16:50:51 -0600

    removed set_base_heuristic function. Added calculate heuristic function

--- Minimax.py
+++ Minimax.py
@@ -1,14 +1,59 @@
 # Reference - https://github.com/lfpelison/ine5430-gomoku/blob/master/src/minimax.py
 
-def set_base_heuristic(board):
-    base_heuristic = [[-999] * board.ncols for _ in range(board.nrows)]
 
-    for row in range(board.nrows):
-        for col in range(board.ncols):
-            if board.board[row][col].is_valid_cell:
-                base_heuristic[row][col] = 0
+UTILITY = {
+    'f': {
+        'captured_e': 500,
+        'captured_g': 250,
+        'neutral': 100
+    },
+    'g_e': {
+        'captured_f': 500,
+        'partial_blocked_f': 250,
+        'neutral': 100
+    }
+}
 
-    return base_heuristic
+
+def calculate_heuristic(board, player, player_c_i, player_c_f, opp_c_i, opp_c_f):
+    # TODO - Code to calculate heuristic value for player based on rules defined
+    """
+    Calculates the heuristic value at the end of the minimax execution
+    :param board: Current Board State
+    :param player: Either Fox 'f' or GeeseElephant 'g_e'
+    :param player_c_i: Player Collection Initial
+    :param player_c_f: Player Collection Final
+    :param opp_c_i: Opponent Collection Initial
+    :param opp_c_f: Opponent Collection Final
+    :return value: numeric heuristic value
+    """
+
+    value = 0
+    if player == 'f':
+        if len(player_c_i.items()) > len(player_c_f.items()):
+            value -= UTILITY['g_e']['captured_f']
+
+        if len(opp_c_i.items()) > len(opp_c_f.items()):
+            animals_captured = [x for x in opp_c_f if x not in opp_c_i]
+
+            for animal in animals_captured:
+                value += UTILITY['f'][f'captured_{animal[0]}']
+        else:
+            value += UTILITY['f']['neutral']
+
+    else:
+        if len(opp_c_i.items()) > len(opp_c_f.items()):
+            value += UTILITY['g_e']['captured_f']
+
+        if len(player_c_i.items()) > len(player_c_f.items()):
+            animals_captured = [x for x in player_c_f if x not in player_c_i]
+
+            for animal in animals_captured:
+                value -= UTILITY['f'][f'captured_{animal[0]}']
+        else:
+            value += UTILITY['g_e']['neutral']
+
+    return value
 
 
 def minimax(state, depth=2):
