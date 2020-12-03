3cc0d56f10770e92d56e9dcd4bb0d9dd316a3370
Sanhita207 <sanhita.dhamdhere5@gmail.com>
2020-12-03 17:48:23 -0600

    Added docstring to Helper.py

--- Helper.py
+++ Helper.py
@@ -4,6 +4,14 @@ import numpy as np
 
 
 def get_single_step_moves(board, row, col):
+    """
+    Returns all possible single step moves (top, bottom, left, right) for the board piece with row and col as its current
+    coordinate
+    :param board: Current Board State
+    :param row: Integer denoting the row coordinate of the board piece
+    :param col: Integer denoting the column coordinate of the board piece
+    :return: List of tuples where each tuple denotes the final coordinate of the board piece after the move
+    """
     adjacent_coordinates = [(row, col - 1), (row, col + 1), (row - 1, col), (row + 1, col)]
 
     nrows = board.nrows
@@ -20,6 +28,13 @@ def get_single_step_moves(board, row, col):
 
 
 def get_hop_moves(board, row, col):
+    """
+    Returns all possible moves that can make it possible for the fox to capture a goose by hopping over it.
+    :param board: Current Board State
+    :param row: Integer denoting the row coordinate of the board piece
+    :param col: Integer denoting the column coordinate of the board piece
+    :return: List of tuples where each tuple denotes the final coordinate of the board piece after the move
+    """
     adjacent_coordinates = [(row, col - 1), (row, col + 1), (row - 1, col), (row + 1, col)]
 
     nrows = board.nrows
@@ -49,6 +64,14 @@ def get_hop_moves(board, row, col):
 
 
 def is_fox_surrounded(board, row, col):
+    """
+    Used to check if the fox is blocked in all direction and is unable to move. Conditions when the fox is not blocked is
+    when it has an empty cell next to it, when it can hop over a goose next to it and when a fox is next to it.
+    :param board: Current Board State
+    :param row: Integer denoting the row coordinate of the board piece
+    :param col: Integer denoting the column coordinate of the board piece
+    :return: Boolean value indicating whether the fox is surrounded
+    """
     if not get_single_step_moves(board, row, col) and not get_hop_moves(board, row, col):
         adjacent_coordinates = [(row, col - 1), (row, col + 1), (row - 1, col), (row + 1, col)]
 
@@ -67,6 +90,14 @@ def is_fox_surrounded(board, row, col):
 
 
 def is_elephant_surrounded(board, row, col):
+    """
+    Used to check if the elephant is surrounded by foxes. An elephant is considered surrounded if there are two foxes in its
+    periphery by occupying top/bottom/left/right cells.
+    :param board: Current Board State
+    :param row: Integer denoting the row coordinate of the board piece
+    :param col: Integer denoting the column coordinate of the board piece
+    :return: Boolean value indicating whether the elephant is surrounded
+    """
     adjacent_coordinates = [(row, col - 1), (row, col + 1), (row - 1, col), (row + 1, col)]
 
     nrows = board.nrows
@@ -85,6 +116,10 @@ def is_elephant_surrounded(board, row, col):
 
 
 def print_board_is_valid(board):
+    """
+    Prints the is_valid_cell parameters of the cells on the board
+    :param board: Game board
+    """
     nrows = len(board)
     ncols = len(board[0])
 
@@ -96,6 +131,10 @@ def print_board_is_valid(board):
 
 
 def print_board_cell_value(board):
+    """
+    Prints the cell_value parameter of the cells on the board
+    :param board: Game board
+    """
     nrows = len(board)
     ncols = len(board[0])
 
@@ -110,12 +149,25 @@ def print_board_cell_value(board):
 
 
 def parse_input_move(move):
+    """
+    Used to split the input into board piece and that board pieces final location
+    :param move: A string containing the move provided by the user
+    :return board_piece: A string containing the name of the board piece to be moved
+    :return board_piece_final_location: A tuple containing the final coordinates of the board piece after the move
+    """
     board_piece = move.split(" ")[0]
     board_piece_final_location = (int(move.split(" ")[1][1]), int(move.split(" ")[1][3]))
     return board_piece, board_piece_final_location
 
 
 def remove_dead_animal(board, row, col, animal_collection):
+    """
+    Removes the captured animal from the board and its corresponding animal collection
+    :param board: Current Board State
+    :param row: Integer denoting the row coordinate of the board piece
+    :param col: Integer denoting the column coordinate of the board piece
+    :param animal_collection: A dictionary containing the board pieces and their locations on the board
+    """
     board.board[row][col].cell_value = None
     val = (row, col)
     required_key = None
@@ -127,6 +179,12 @@ def remove_dead_animal(board, row, col, animal_collection):
 
 
 def remove_dead_foxes_and_elephants(board, fox_collection, elephant_collection):
+    """
+    Removes the foxes and elephants from the game if either of them are surrounded
+    :param board: Current Board State
+    :param fox_collection: A dictionary containing the fox board pieces and their locations on the board
+    :param elephant_collection: A dictionary containing the elephant board pieces and their locations on the board
+    """
     for fox_row, fox_col in list(fox_collection.values()):
         if is_fox_surrounded(board, fox_row, fox_col):
             remove_dead_animal(board, fox_row, fox_col, fox_collection)
